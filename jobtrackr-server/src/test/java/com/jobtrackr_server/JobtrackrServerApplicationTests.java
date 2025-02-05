5481f5147400822bd6368cbea90a07533c8faea0
Vikas Dangi <mvikasdangi@gmail.com>
2025-02-06 01:31:40 +0530

    feat: initialized backend spring application

---
+++ jobtrackr-server/src/test/java/com/jobtrackr_server/JobtrackrServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobtrackr_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackrServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
