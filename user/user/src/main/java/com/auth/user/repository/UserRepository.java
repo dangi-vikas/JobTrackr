2cff35001f9d4fd38add863b86808eafd7fee350
Vikas Dangi <mvikasdangi@gmail.com>
2025-02-09 03:08:16 +0530

    feat: added repository method

---
+++ user/user/src/main/java/com/auth/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.auth.user.repository;
+
+import com.auth.user.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+}
